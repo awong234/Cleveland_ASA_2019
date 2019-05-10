fb6431111ee7b6df9a1fc0c67d4118dc03938233
Alec Wong <aw685@cornell.edu>
2019-05-10 15:26:12 -0400

    Added more examples scraping; nps

---
+++ web_scrape.R
@@ -0,0 +1,58 @@
+library(XML)
+library(RCurl)
+library(dplyr)
+
+# Billboard top 100 -- scrape from date
+
+url_text = "https://www.billboard.com/charts/hot-100/1991-08-05"
+
+getURL(url = url_text) %>% 
+  htmlParse %>% 
+  xpathSApply(path = "//*/div[@class='chart-list-item__title']/span[@class='chart-list-item__title-text']",
+              xmlValue)
+
+
+# NPS example -- scrape mailing address
+
+url_text = "https://www.nps.gov/cuva/index.htm"
+
+getURL(url = url_text) %>% 
+  htmlParse %>% 
+  xpathSApply(path = "//*/span[@itemprop='streetAddress']",
+              xmlValue)
+
+# Get all available park pages
+
+url_ref = "https://www.nps.gov/findapark/index.htm"
+
+all_codes = getURL(url = url_ref) %>% 
+  htmlParse %>% 
+  xpathSApply(path = "//*/select[@id='alphacode']/optgroup/option",
+              xmlGetAttr, "value")
+
+all_codes
+
+# Reference from source:
+
+# <select size='2' id='alphacode' name='alphacode' class=' js-multiselect-findapark'  data-nonSelectedText='Park Name' data-enableFiltering='true' >
+#   <optgroup label='A'>
+#   <option value='abli'>Abraham Lincoln Birthplace National Historical Park</option>
+
+# loop over each of these
+
+getAddr = function(code){
+  
+  url_text = paste0("https://www.nps.gov/", code, "/index.htm")
+  
+  item = tryCatch(expr = {
+    getURL(url = url_text) %>% 
+    htmlParse %>% 
+    xpathSApply(path = "//*/span[@itemprop='streetAddress']",
+                xmlValue)
+  }, error = function(m){message(m); return(NULL)}
+  )
+  
+  return(item)
+}
+
+sapply(X = all_codes, FUN = getAddr)
